3c63628b355b74d2e2201173d1efc5aa9e360f2d
symias <sasbury2@huskers.unl.edu>
2021-06-08 08:55:38 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sydney Asbury
+ * 
+ * Date: 6/8/2021
+ * 
+ * Another simple hello world program.
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+    printf("Hello, my name is Sydney!\n"); 
+     
+    return 0;
+ }
